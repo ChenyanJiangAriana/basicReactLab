35c7b60ce11387c1e046cf9963c28c227bc7f532
Jiang Chenyan <624071310@qq.com>
2020-10-18 15:26:38 +0100

    Exercise 3 completed

--- components/exercises/03_props.js
+++ components/exercises/03_props.js
@@ -1,11 +1,53 @@
+import { storiesOf } from "@storybook/react";
 import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+//import CourseModulesWithProps from "../components/exercises/03_props";
 
+const Demo = (props) => {
+  const course = "MSc Computing - Modules table.";
+  const modules = [
+    {
+      name: "DevOps",
+      noLectures: 2,
+      noPracticals: 3
+    },
+    {
+      name: "Enterprise Web Dev",
+      noLectures: 3,
+      noPracticals: 2
+    }
+  ];
+  
+      return (
+        <div>
+          <h1>{props.course}</h1>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+              <th>name</th>
+              <th>no lectures</th>
+              <th>no Practicals</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{props.modules[0].name}</td>
+                <td>{props.modules[0].noLectures}</td>
+                <td>{props.modules[0].noPracticals}</td>
+              </tr>
+              <tr>
+                <td>{props.modules[1].name}</td>
+                <td>{props.modules[1].noLectures}</td>
+                <td>{props.modules[1].noPracticals}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    // storiesOf("Exercises", module)
+    //   .add("03 - component with props", () => {
+    //     const twoModules = [modules[0], modules[1]];
+    //     return <CourseModulesWithProps modules={twoModules} course={name} />;
+    //   })
 
-const Demo = () => {
-  return (
-      <h1>TODO</h1>
-  );
-};
-
-export default Demo;
+ };export default Demo
